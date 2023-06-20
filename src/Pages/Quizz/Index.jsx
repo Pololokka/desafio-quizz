c1ca30559a2ec94d63767b27bc519be11011a85b
Vitor Bulbovas <vitorbulbovas1@hotmail.com>
2023-06-20 20:19:43 -0300

    :construction: feat: fazendo exibição de perguntas

--- src/Pages/Quizz/Index.jsx
+++ src/Pages/Quizz/Index.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 function Quizz() {
+  const [question, setQuestion] = useState([]);
+  const [answers, setAnswers] = useState("");
+  const [amt, setAmt] = useState(2);
+
+  const getQuestion = async () => {
+    try {
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=${amt}`
+      );
+
+      const data = response.data.results;
+
+      setQuestion(data);
+      console.log(question);
+      console.log(data);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getQuestion();
+    setQuestion("teste");
+  }, []);
+
   return (
     <>
       <main>
         <h1 className="titulo titulo-hover">QUIZZ</h1>
         <h2 className="subtitulo subtitulo-hover">QUIZZ</h2>
+        {/* {question?.map((element, index) => {
+          <p key={index} className="texto">
+            {element}
+          </p>;
+        })} */}
       </main>
     </>
   );
